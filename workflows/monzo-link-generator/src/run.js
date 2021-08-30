2d8782ccc7b6c8a9f53bfd3c293345192d7059dc
Robb Knight <desk@rknight.me>
2021-08-30 12:28:43 +0100

    Fix monzo generation

--- workflows/monzo-link-generator/src/run.js
+++ workflows/monzo-link-generator/src/run.js
@@ -1,9 +1,10 @@
-var username = $.getenv('MONZOUSERNAME');
-
 function run(argv) {
+  ObjC.import('stdlib');
   var input = argv[0].split(' ');
   var price = input[0];
   var description = input[1] ? input[1] : null;
+
+  var username = $.getenv('MONZOUSERNAME');
 
   var url = 'https://monzo.me/' + username + '/' + price;
 
