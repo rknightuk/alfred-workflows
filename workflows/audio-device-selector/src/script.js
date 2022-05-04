e771fe2350b642a64dcdb8287690cf3c96a70cc3
Robb Knight <desk@rknight.me>
2022-05-04 16:02:03 +0100

    updates to ads

--- workflows/audio-device-selector/src/script.js
+++ workflows/audio-device-selector/src/script.js
@@ -1,3 +1,32 @@
+function formatData(title) {
+	const names = {
+		'DELL U2419H': {
+			name: 'Headphones',
+			icon: 'headphones.png'
+		},
+		'USB Audio Device': {
+			name: 'Speakers',
+			icon: 'speaker.png',
+		},
+		'MacBook Pro Speakers': {
+			name: 'Vision',
+			icon: 'laptop.png'
+		},
+		'MacBook Pro Microphone': {
+			name: 'Vision',
+			icon: 'laptop.png'
+		}
+	}
+
+	return names[title] ?? { name: title, icon: null }
+
+	if (Object.keys(names).includes(title)) {
+		return names[title]
+	}
+
+	return title
+}
+
 function run(argv) {
 	const type = argv[0]
 
@@ -7,10 +36,14 @@ function run(argv) {
 	devices = app.doShellScript(`/usr/local/bin/SwitchAudioSource -a -t ${type}`);
 
 	items = devices.split('\r').map(d => {
+		const data = formatData(d)
 	    return {
-	    	title: d,
+	    	title: data.name,
 	    	arg: `-s "${d}" -t ${type}`,
 	    	autocomplete: d,
+	    	icon: {
+	    		path: data.icon ?? null,
+	    	}
 	    }
 	})
 
