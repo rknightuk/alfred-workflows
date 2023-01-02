0c0b5b68c1df546889c84795f3538ffd75d86d89
Robb Knight <desk@rknight.me>
2023-01-02 12:15:09 +0000

    Bunch of other fixes

--- workflows/fathom-analytics/src/scripts/clear_cache.js
+++ workflows/fathom-analytics/src/scripts/clear_cache.js
@@ -1,7 +1,9 @@
 function run(argv) {
 	const app = Application.currentApplication();
+	ObjC.import("stdlib")
 	app.includeStandardAdditions = true;
 
 	const cacheFile = Path(`${$.getenv('alfred_workflow_data')}/site_cache.json`)
+	console.log(cacheFile)
 	app.doShellScript(`rm -rf "${cacheFile}"`)
 }
