4809f2bf84b3779405f5133af428e4ae8aadb62f
Matt Henkel <mhenkel@proofpoint.com>
2024-01-12 15:55:48 -0700

    Add event `mods` actions.
    1. CMD to open the `meeting_url`
    2. ALT to copy the title to the clipboard
    3. Allow opening events in BusyCal
       Add the `use_busycal` environment variable.

--- workflows/agenda/src/scripts/events/lib/upcoming.js
+++ workflows/agenda/src/scripts/events/lib/upcoming.js
@@ -1,133 +1,149 @@
 var UpcomingEvents = (function() {
-	return {
-		get: function(todayOnly, listName, limit) {
-			var app = Application.currentApplication();
-			app.includeStandardAdditions = true;
+  return {
+    get: function(todayOnly, listName, limit) {
+      var app = Application.currentApplication();
+      app.includeStandardAdditions = true;
 
-		  	const events = app.doShellScript(`./agendas events${listName ? ` "${listName}"`: ''} --limit ${limit};`)
+      const events = app.doShellScript(`./agendas events${listName ? ` "${listName}"`: ''} --limit ${limit};`)
 
-		  	const todayTS = new Date()
-		  	const todayDate = todayTS.getDate() < 10 ? `0${todayTS.getDate()}` : todayTS.getDate();
-		  	const todayMonth = (todayTS.getMonth() + 1) < 10 ? `0${(todayTS.getMonth() + 1)}` : todayTS.getMonth() + 1;
-		  	const todayDateString = `${todayTS.getFullYear()}-${todayMonth}-${todayDate}`
+      const todayTS = new Date()
+      const todayDate = todayTS.getDate() < 10 ? `0${todayTS.getDate()}` : todayTS.getDate();
+      const todayMonth = (todayTS.getMonth() + 1) < 10 ? `0${(todayTS.getMonth() + 1)}` : todayTS.getMonth() + 1;
+      const todayDateString = `${todayTS.getFullYear()}-${todayMonth}-${todayDate}`
 
-		  	const tomorrowTS = new Date()
-		  	tomorrowTS.setDate(tomorrowTS.getDate() + 1)
-		  	const tomorrowDate = tomorrowTS.getDate() < 10 ? `0${tomorrowTS.getDate()}` : tomorrowTS.getDate();
-		  	const tomorrowMonth = (tomorrowTS.getMonth() + 1) < 10 ? `0${(tomorrowTS.getMonth() + 1)}` : tomorrowTS.getMonth() + 1;
-		  	const tomorrowDateString = `${tomorrowTS.getFullYear()}-${tomorrowMonth}-${tomorrowDate}`
+      const tomorrowTS = new Date()
+      tomorrowTS.setDate(tomorrowTS.getDate() + 1)
+      const tomorrowDate = tomorrowTS.getDate() < 10 ? `0${tomorrowTS.getDate()}` : tomorrowTS.getDate();
+      const tomorrowMonth = (tomorrowTS.getMonth() + 1) < 10 ? `0${(tomorrowTS.getMonth() + 1)}` : tomorrowTS.getMonth() + 1;
+      const tomorrowDateString = `${tomorrowTS.getFullYear()}-${tomorrowMonth}-${tomorrowDate}`
 
-	  		const eventData = events === '' ? [] : events.split('\r')
-	  		let items = []
+      const eventData = events === '' ? [] : events.split('\r')
+      let items = []
 
-	  		eventData.forEach(ri => {
-	  			let parsed = JSON.parse(ri)
-	  			parsed = {
-	  				...parsed,
-	  				allDay: parsed.allDay === 'true',
-	  			}
+      eventData.forEach(ri => {
+        let parsed = JSON.parse(ri)
+        parsed = {
+          ...parsed,
+          allDay: parsed.allDay === 'true',
+        }
 
-	  			let startDate = parsed.start
-	  		    let endDate = parsed.end
+        let startDate = parsed.start
+        let endDate = parsed.end
 
-	  			const eventTS = new Date(parsed.start.replace(' ', 'T'))
-	  			const isOverdue = eventTS < todayTS
-	  			const isToday = parsed.start.includes(todayDateString)
-	  			const isTomorrow = parsed.start.includes(tomorrowDateString)
+        const eventTS = new Date(parsed.start.replace(' ', 'T'))
+        const isOverdue = eventTS < todayTS
+        const isToday = parsed.start.includes(todayDateString)
+        const isTomorrow = parsed.start.includes(tomorrowDateString)
 
-	  			const [eventStartDate, eventStartTime] = startDate.split(' ')
-	  		    const [eventEndDate, eventEndTime] = endDate.split(' ')
+        const [eventStartDate, eventStartTime] = startDate.split(' ')
+        const [eventEndDate, eventEndTime] = endDate.split(' ')
 
-	  			const isAllDay = parsed.allDay
-	  		    const isSameDay = !isAllDay && eventStartDate === eventEndDate
+        const isAllDay = parsed.allDay
+        const isSameDay = !isAllDay && eventStartDate === eventEndDate
 
-	  		   	if (todayOnly && !parsed.start.includes(todayDateString)) return
-	  	   		let dateOutput = parsed.allDay ? 'All Day' : eventStartTime
-	  	   		let sort = parsed.allDay ? 'All Day' : eventStartTime
-	  	   		if (!todayOnly) {
-	  	   			sort = parsed.start
-	  	   			if (parsed.allDay)
-	  	   			{
-	  	   				dateOutput = `${eventStartDate} All Day`
-	  	   			} else {
-	  					dateOutput = parsed.start
-	  	   			}
-	  	   		}
+        const calendarURI = encodeURIComponent(parsed.calendar)
+        const titleURI = encodeURIComponent(parsed.title)
+        const eventTSisoSeconds = encodeURIComponent(eventTS.toISOString()).replace('.000Z', 'Z')
 
-	  	   		let icon = 'calendar'
-	  	   		const titleLC = parsed.title.toLowerCase()
-	  		    if (titleLC.includes('birthday'))
-	  		    {
-	  		    	icon = 'birthday'
-	  		    } else if (titleLC.includes('holiday'))
-	  		    {
-	  		    	icon = 'holiday'
-	  		    }
+        if (todayOnly && !parsed.start.includes(todayDateString)) return
+        let dateOutput = parsed.allDay ? 'All Day' : eventStartTime
+        let sort = parsed.allDay ? 'All Day' : eventStartTime
+        if (!todayOnly) {
+          sort = parsed.start
+          if (parsed.allDay)
+          {
+            dateOutput = `${eventStartDate} All Day`
+          } else {
+            dateOutput = parsed.start
+          }
+        }
 
-	  		     if (isToday)
-	  		    {
-	  				startDate = parsed.start.replace(todayDateString, 'Today at')
-	  				icon = 'today'
-	  		    } else if (isTomorrow)
-	  		    {
-	  		    	startDate = parsed.start.replace(tomorrowDateString, 'Tomorrow at')
-	  		    }
+        let icon = 'calendar'
+        const titleLC = parsed.title.toLowerCase()
+        if (titleLC.includes('birthday'))
+        {
+          icon = 'birthday'
+        } else if (titleLC.includes('holiday'))
+        {
+          icon = 'holiday'
+        }
 
-	  		    startDate = startDate.replace(' at 00:00', '')
+        if (isToday)
+        {
+          startDate = parsed.start.replace(todayDateString, 'Today at')
+          icon = 'today'
+        } else if (isTomorrow)
+        {
+          startDate = parsed.start.replace(tomorrowDateString, 'Tomorrow at')
+        }
 
-	  		    let subtitle = `${startDate}`
-	  		    if (isAllDay) {
-	  		    	subtitle = 'All Day'
-	  		    	if (isToday) {
-	  		    		subtitle += ' Today'
-	  		    	} else if (isTomorrow) {
-	  					subtitle += ' Tomorrow'
-	  		    	} else {
-	  		    		subtitle = `${eventStartDate} All Day`
-	  		    	}
-	  		    } else if (isSameDay && isToday) {
-	  		    	subtitle += ` until ${eventEndTime}`
-	  		    } else if (isSameDay && isTomorrow)
-	  		    {
-	  		    	subtitle += ` - ${eventEndTime}`
-	  		    	subtitle = subtitle.replace('Tomorrow at', 'Tomorrow')
-	  		    } else if (!isSameDay) {
-	  		    	subtitle += ` - ${eventEndDate} ${eventEndTime}`
-	  		    } else {
-	  		    	subtitle += ` - ${eventEndTime}`
-	  		    }
+        startDate = startDate.replace(' at 00:00', '')
 
-	  		    if (parsed.location)
-	  		    {
-	  		    	subtitle += ` @ ${parsed.location}`
-	  		    }
-	  		    
-	  		    subtitle = `${subtitle}${listName ? '' : ` / ${parsed.calendar}`} `
+        let subtitle = `${startDate}`
+        if (isAllDay) {
+          subtitle = 'All Day'
+          if (isToday) {
+            subtitle += ' Today'
+          } else if (isTomorrow) {
+            subtitle += ' Tomorrow'
+          } else {
+            subtitle = `${eventStartDate} All Day`
+          }
+        } else if (isSameDay && isToday) {
+          subtitle += ` until ${eventEndTime}`
+        } else if (isSameDay && isTomorrow)
+        {
+          subtitle += ` - ${eventEndTime}`
+          subtitle = subtitle.replace('Tomorrow at', 'Tomorrow')
+        } else if (!isSameDay) {
+          subtitle += ` - ${eventEndDate} ${eventEndTime}`
+        } else {
+          subtitle += ` - ${eventEndTime}`
+        }
 
-	  		    if (todayOnly) subtitle = subtitle.replace('Today at ', '')
+        if (parsed.location)
+        {
+          subtitle += ` @ ${parsed.location}`
+        }
 
-  		    	items.push({
-  		    		title: parsed.title,
-  		    		subtitle,
-  		    		arg: `c:${eventStartDate}`,
-  		    		autocomplete: parsed.title,
-  		    		icon: {
-  		    			path: `icons/${icon}.png`,
-  		    		},
-  		    		sort,
-  		    	})
-	  		})
+        subtitle = `${subtitle}${listName ? '' : ` / ${parsed.calendar}`} `
 
-		  	return items
-		},
-	};
+        if (todayOnly) subtitle = subtitle.replace('Today at ', '')
+
+        busycalURI = encodeURI(`busycalevent://find/${parsed.calendar}/${parsed.title}/${eventTSisoSeconds}`)
+
+        items.push({
+          title: parsed.title,
+          subtitle,
+          arg: $.getenv('use_busycal') ? `c:${busycalURI}` : `c:${eventStartDate}`,
+          autocomplete: parsed.title,
+          icon: {
+            path: `icons/${icon}.png`,
+          },
+          sort,
+          mods: {
+            cmd: {
+              arg: parsed.meeting_url,
+              subtitle: `Join ${parsed.meeting_url}`
+            },
+            alt: {
+              arg: `${parsed.title}`,
+              subtitle: `Copy title to clipboard`
+            }
+          }
+        })
+      })
+
+      return items
+    },
+  };
 })();
 
 // Add AMD compatibility.
 if (typeof define === 'function' && define.amd) {
-	define(UpcomingEvents);
+  define(UpcomingEvents);
 }
 // Add CommonJS compatibility.
 else if (typeof module !== 'undefined' && module.exports) {
-	module.exports = UpcomingEvents;
+  module.exports = UpcomingEvents;
 }
